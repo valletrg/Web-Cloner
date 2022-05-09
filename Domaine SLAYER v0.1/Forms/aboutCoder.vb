2049ac108ff5c29620618424cd9b7094315491bd
valletrg <94022369+valletrg@users.noreply.github.com>
2022-05-08 23:21:34 -0600

    Add files via upload

---
+++ Domaine SLAYER v0.1/Forms/aboutCoder.vb
@@ -0,0 +1,19 @@
+Public Class aboutCoder
+
+    Private Sub LinkLabel1_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkLabel1.LinkClicked
+        Process.Start("https://t.me/SLAYERS404")
+
+    End Sub
+
+    Private Sub LinkLabel2_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkLabel2.LinkClicked
+        Process.Start("https://www.youtube.com/c/XSLAYERTN")
+    End Sub
+
+    Private Sub LinkLabel3_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkLabel3.LinkClicked
+        Process.Start("https://github.com/X-SLAYER")
+    End Sub
+
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
+
+    End Sub
+End Class
